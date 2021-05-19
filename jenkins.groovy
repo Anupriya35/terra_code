df41cd3c5991e3953e21374148fa8923f867bc98
Anupriya35 <84177858+Anupriya35@users.noreply.github.com>
2021-05-19 21:20:35 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,5 +1,6 @@
 
 pipeline {
+	
 parameters {
         string(name: 'environment', defaultValue: 'terraform', description: 'Workspace/environment file to use for deployment')
         booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
@@ -26,7 +27,15 @@ stage('terraform init') {
                 
 		            }
         }
-
+stage('terraform Plan') {
+            steps {
+               
+                sh 'pwd;cd terraform ; terraform workspace new ${environment}'
+                sh 'pwd;cd terraform ; terraform workspace select ${environment}'
+                sh "pwd;cd terraform ;terraform plan -input=false -out tfplan "
+                sh 'pwd;cd terraform ;terraform show -no-color tfplan > tfplan.txt'
+            }
+        }
        
         }
   
